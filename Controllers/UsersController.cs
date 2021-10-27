dce6660ec5005b6d223ac892ecba5c3c1aa0a239
kolyankolegov@gmail.com <kolyankolegov@gmail.com>
2021-10-27 09:06:00 +0500

    Изменения

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -7,7 +7,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace EducateApp.Controllers
+namespace Texnicum.Controllers
 {
     public class UsersController : Controller
     {
