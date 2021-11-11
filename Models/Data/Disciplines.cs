c6c9f6d91bd1bf9022652f1675dcf1f096b494cf
kolyankolegov@gmail.com <kolyankolegov@gmail.com>
2021-11-11 09:08:29 +0500

    Добавлены 2 таблицы "Disciplines" и "TypesOfTotals"

---
+++ Models/Data/Disciplines.cs
@@ -0,0 +1,43 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Texnicum.Models.Data
+{
+    public class Disciplines
+    {
+        // Key - поле первичный ключ
+        // DatabaseGenerated(DatabaseGeneratedOption.Identity) - поле автоинкреметное
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Display(Name = "ИД")]
+        public short Id { get; set; }
+
+        [Required]
+        [Display(Name = "Индекс профессионального модуля")]
+        public string IndexProfModule { get; set; }
+
+        [Required]
+        [Display(Name = "Название профессионального модуля")]
+        public string  ProfModule{ get; set; }
+
+        [Required]
+        [Display(Name = "Индекс")]
+        public string Index { get; set; }
+
+        [Required(ErrorMessage = "Введите название")]
+        [Display(Name = "Название")]
+        public string Name { get; set; }
+
+        [Required]
+        [Display(Name = "Краткое название")]
+        public string ShortName { get; set; }
+
+        // так как у каждого пользователя (преподавателя) свой список форм обучения, то нужно указывать внешний ключ
+        [Required]
+        public string IdUser { get; set; }
+
+        // Навигационные свойства
+        // свойство нужно для более правильного отображения данных в представлениях
+        [ForeignKey("IdUser")]
+        public User User { get; set; }
+    }
+}
---
+++ Models/Data/TypesOfTotals.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Texnicum.Models.Data
+{
+    public class TypesOfTotals
+    {
+        // Key - поле первичный ключ
+        // DatabaseGenerated(DatabaseGeneratedOption.Identity) - поле автоинкреметное
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Display(Name = "ИД")]
+        public short Id { get; set; }
+
+        [Required(ErrorMessage = "Введите название аттестации")]
+        [Display(Name = "Название аттестации")]
+        public string CertificateName { get; set; }
+
+        // так как у каждого пользователя (преподавателя) свой список форм обучения, то нужно указывать внешний ключ
+        [Required]
+        public string IdUser { get; set; }
+
+        // Навигационные свойства
+        // свойство нужно для более правильного отображения данных в представлениях
+        [ForeignKey("IdUser")]
+        public User User { get; set; }
+    }
+}
