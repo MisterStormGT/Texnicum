dd1e9e6a847bc15510dde1d0299fd8f9e6ea577b
kolyankolegov@gmail.com <kolyankolegov@gmail.com>
2021-11-26 21:58:37 +0500

    Почта

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -55,7 +55,7 @@ namespace Texnicum.Controllers
                         protocol: HttpContext.Request.Scheme);
                     EmailService emailService = new EmailService();
                     await emailService.SendEmailAsync(model.Email, "Confirm your account",
-                        $"Подтвердите регистрацию, перейдя по ссылке: <a href='{callbackUrl}'>link</a>");
+                        $"Подтвердите регистрацию, перейдя по <a href='{callbackUrl}'>ссылке</a>");
 
                     return View("RegisterConfirmation");
                 }
