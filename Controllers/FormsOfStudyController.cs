66b10c7c09b4c5bc870cbb02c3d41035815ea7ac
kolyankolegov@gmail.com <kolyankolegov@gmail.com>
2021-11-27 09:39:58 +0500

    Добавление сортировки для Форм обучения

--- Controllers/FormsOfStudyController.cs
+++ Controllers/FormsOfStudyController.cs
@@ -4,6 +4,7 @@ using Texnicum.ViewModels.FormsOfStudy;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using System.Linq;
 using System.Threading.Tasks;
@@ -25,19 +26,26 @@ namespace Texnicum.Controllers
         }
 
         // GET: FormsOfStudy
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(FormOfStudySortState sortOrder = FormOfStudySortState.FormOfEduAsc)
         {
             // находим информацию о пользователе, который вошел в систему по его имени
             IdentityUser user = await _userManager.FindByNameAsync(HttpContext.User.Identity.Name);
 
             // через контекст данных получаем доступ к таблице базы данных FormsOfStudy
-            var appCtx = _context.FormsOfStudy
+            var formsOfStudy = _context.FormsOfStudy
                 .Include(f => f.User)                // и связываем с таблицей пользователи через класс User
-                .Where(f => f.IdUser == user.Id)     // устанавливается условие с выбором записей форм обучения текущего пользователя по его Id
-                .OrderBy(f => f.FormOfEdu);          // сортируем все записи по имени форм обучения
+                .Where(f => f.IdUser == user.Id);     // устанавливается условие с выбором записей форм обучения текущего пользователя по его Id
+
+            ViewData["FormOfEduSort"] = sortOrder == FormOfStudySortState.FormOfEduAsc ? FormOfStudySortState.FormOfEduDesc : FormOfStudySortState.FormOfEduAsc;
+
+            formsOfStudy = sortOrder switch
+            {
+                FormOfStudySortState.FormOfEduDesc => formsOfStudy.OrderByDescending(s => s.FormOfEdu),
+                _ => formsOfStudy.OrderBy(s => s.FormOfEdu),
+            };
 
             // возвращаем в представление полученный список записей
-            return View(await appCtx.ToListAsync());
+            return View(await formsOfStudy.AsNoTracking().ToListAsync());
         }
 
         // GET: FormsOfStudy/Create
@@ -107,15 +115,6 @@ namespace Texnicum.Controllers
             if (id != formOfStudy.Id)
             {
                 return NotFound();
-            }
-
-            IdentityUser user = await _userManager.FindByNameAsync(HttpContext.User.Identity.Name);
-
-            if (_context.FormsOfStudy
-                .Where(f => f.IdUser == user.Id &&
-                    f.FormOfEdu == model.FormOfEdu).FirstOrDefault() != null)
-            {
-                ModelState.AddModelError("", "Введеная форма обучения уже существует");
             }
 
             if (ModelState.IsValid)
